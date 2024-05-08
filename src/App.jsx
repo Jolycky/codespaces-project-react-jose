f16804391c558ecb8c7b3929de2f3ec58257a33d
Jose Bagus Ramadhan <126927999+Jolycky@users.noreply.github.com>
2024-05-08 10:57:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Jose Bagus Ramadhan",
+  title: "Software Engineer",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  gitHub: "github/jolycky",
+  instagram: "jo.whnt",
+  linkedIn: "josebagus",
 };
 
 const primaryColor = "#4E567E";
